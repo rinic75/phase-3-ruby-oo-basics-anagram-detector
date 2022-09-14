1fa550e51d8722771b929d1ec963fc9e7a137359
Chang Shin <cshin7374@gmail.com>
2022-09-13 21:39:36 -0500

    commit tree ff5a9bdb0923c43144151557121650586e44644e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,9 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word=word    
+  end
+  def match(array)
+    array.select {|w| w.split("").sort == @word.split("").sort}
+  end
+end
